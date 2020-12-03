a137369809b426781bb854494a360131794c4d38
Emre Erdog <emreerdog@hotmail.com>
2020-12-03 17:14:51 +0300

    Product upload nearly made, only writing to database part left

--- LimdropMain/controllers/uploads.cc
+++ LimdropMain/controllers/uploads.cc
@@ -34,6 +34,15 @@ void uploads::upload_form(const HttpRequestPtr& req,std::function<void (const Ht
 
 
 void uploads::upload_files(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback){
+	drogon::MultiPartParser mpp;
+	mpp.parse(req);
+	const std::vector<drogon::HttpFile> f = mpp.getFiles();
+	std::cout << f[1].getFileName() << std::endl;
+	std::cout << f[2].getFileName() << std::endl;
+	std::cout << f[3].getFileName() << std::endl;
+	std::cout << f[4].getFileName() << std::endl;
+
+	
 
 }
 
@@ -42,14 +51,31 @@ void uploads::files_page(const HttpRequestPtr& req,std::function<void (const Htt
 	std::unordered_map<std::string, std::string> tempParam = req->parameters();
 	ManualPatternFiller MPF(1, "mark");
 	std::string tempContent = MPF.fillPatterns("addimage.html", "");
-	std::vector<std::string> text;
-	
+	std::vector<std::string> unchangable;
+	std::vector<std::string> fileInputs;
+	unsigned short imageCount = 0;
+
 	for(const auto& n : tempParam){
-		text.push_back(n.second);
+		if(n.first == "imgCount"){
+			imageCount = std::stoi(n.second);
+		}
+		std::string inputs = "<p>";
+		inputs += n.first + ": <br>" + n.second + "<br><p><br>";
+		unchangable.push_back(inputs);
+	}
+	for(int i = 0; i < imageCount; i++){
+		std::string formElement = "<input type='file' name='" + drogon::utils::genRandomString(8) + "'>";
+		fileInputs.push_back(formElement);
 	}
 
+	std::reverse(unchangable.begin(), unchangable.end());
+	fileInputs.push_back("<input type='submit' value='Upload image'>");
+	unchangable.insert(unchangable.end(), fileInputs.begin(), fileInputs.end());
+
 	SequentialPatternFiller SPF;
-	std::string content = SPF.fillOnString(tempContent, "#$ozan", text, "form", true, true, "/uploads/upload-file", "", "post");
+	std::string content = SPF.fillOnString(tempContent, "#$ozan", unchangable, "form", true, true, "/uploads/product/upload-photo", "", "post", true);
+	fileInputs.clear();
+	unchangable.clear();
 	auto resp = HttpResponse::newHttpResponse();
 	resp->setBody(content);
 	callback(resp);
